bf75a96e1bd449a6a9b8d5567072ab196ba55fc4
beratyldr <beratyldr.21@gmail.com>
2021-12-01 19:52:30 +0300

    .

--- routes/api.php
+++ routes/api.php
@@ -17,19 +17,20 @@ use Illuminate\Support\Facades\Route;
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
-Route::group([ 'middleware' => ['auth:api','cors']], function () {
 
-    Route::apiResources([
-        "expenses" => "ExpensesController",
-        "auth" => "AuthController",
-        "category" =>"CategoryController"
-    ]);
-    Route::group(['prefix' => 'user'], function() {
-        Route::get('/', function() {
-            return auth()->guard('api')->user();
-        });
+//Route::group([ 'middleware' => 'auth:api'], function () {});
+
+    
+Route::group(['prefix' => 'user'], function() {
+    Route::get('/', function() {
+        return auth()->guard('api')->user();
     });
- 
-
 });
+
+
+Route::apiResources([
+    "expenses" => "ExpensesController",
+    "auth" => "AuthController",
+    "category" =>"CategoryController"
+]);
 Route::post('login','AuthController@login');
