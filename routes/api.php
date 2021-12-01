a4d89c5a2831a6b92cab5cf777207cbc42167ef6
beratyldr <beratyldr.21@gmail.com>
2021-12-01 18:39:21 +0300

    cors

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
-Route::group([ 'middleware' => 'auth:api'], function () {
+Route::group([ 'middleware' => ['auth:api','cors']], function () {
 
     Route::apiResources([
         "expenses" => "ExpensesController",
@@ -29,6 +29,7 @@ Route::group([ 'middleware' => 'auth:api'], function () {
             return auth()->guard('api')->user();
         });
     });
-    Route::post('login','AuthController@login');
+ 
 
 });
+Route::post('login','AuthController@login');
